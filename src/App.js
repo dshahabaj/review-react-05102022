4eeb80b39aea348bc624dbafe578ae42ee78a12a
Shahabaj Dange <dangeshahabaj@hotmail.com>
2022-10-05 04:11:47 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+import Review from './Review';
+
+export default function App() {
+  return (
+    <div className="App">
+     <main>
+      <section className='container'>
+      <div className='title'>
+        <h2>our reviews</h2>
+        <div className='underline'></div>
+      </div>
+      <Review />
+      </section>
+     </main>
+    </div>
+  );
+}
